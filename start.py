3b227dac923a9baaad697a8055de8f0947e60161
Brandon Wiley <brandon@blanu.net>
2014-08-21 10:31:56 -0700

    Install stan dependencies on start

---
+++ start.py
@@ -0,0 +1,24 @@
+import logging
+import subprocess
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+from generic import GenericPage
+
+class Start(GenericPage):
+  def execute(self, method, user, req, resp, args):
+    logging.info('Start')
+    subprocess.call(['sudo', 'pip', 'install', 'cython'])
+    subprocess.call(['sudo', 'pip', 'install', 'numpy'])
+    subprocess.call(['sudo', 'pip', 'install', 'pystan'])
+
+app = webapp.WSGIApplication([
+  ('/_ah/start', Start),
+], debug=True)
+
+def main():
+  run_wsgi_app(application)
+
+if __name__ == "__main__":
+  main()
